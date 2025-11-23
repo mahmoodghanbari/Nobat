9ae5a20c7e603855979d7ddb155106dbbed03c18
MahmoodGhanbary <code.mahmood@gmail.com>
2025-11-23 13:45:16 +0330

    feat: add NobatDbContext with EF Core and PostgreSQL support

---
+++ src/Backend/src/Nobat.Backend.Infrastructure/Persistence/NobatDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobat.Backend.Infrastructure.Persistence
+{
+    public sealed class NobatDbContext : DbContext
+    {
+        public NobatDbContext(DbContextOptions<NobatDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(NobatDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.EnableSensitiveDataLogging();
+                optionsBuilder.EnableDetailedErrors();
+            }
+        }
+    }
+}
